96b4c873c8209e6694ce2b93222a7e4fb1855475
Michael Weitner <michael@weitnerembeddedsystem.com>
2022-04-03 19:16:49 +0200

    changed terminal and i3 font
    - installed dunst notification system

--- install.sh
+++ install.sh
@@ -2,29 +2,40 @@
 
 # share entire X11 directory
 rm -rf "$HOME/.config/X11"
-ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+ln -s "$DOTFILES/X11" "$HOME/.config"
 
 ######
 # i3 #
 ######
 
 rm -rf "$HOME/.config/i3"
-ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+ln -s "$DOTFILES/i3" "$HOME/.config"
 
 #######
 # zsh #
 #######
 mkdir -p "$HOME/.config/zsh"
-ln -sf "$HOME/dotfiles/zsh/.zshenv" "$HOME"
-ln -sf "$HOME/dotfiles/zsh/.zshrc" "$HOME/.config/zsh"
+ln -sf "$DOTFILES/zsh/.zshenv" "$HOME"
+ln -sf "$DOTFILES/zsh/.zshrc" "$HOME/.config/zsh"
 
-ln -sf "$HOME/dotfiles/zsh/aliases" "$HOME/.config/zsh"
+ln -sf "$DOTFILES/zsh/aliases" "$HOME/.config/zsh"
 
 #make sure entire folder external is symlinked
 rm -rf "$HOME/.config/zsh/external"
-ln -sf "$HOME/dotfiles/zsh/external" "$HOME/.config/zsh"
+ln -sf "$DOTFILES/zsh/external" "$HOME/.config/zsh"
 
 #SpaceVim global config
 rm -rf "$HOME/.SpaceVim.d"
-ln -sf "$HOME/dotfiles/spacevim/.SpaceVim.d" "$HOME/"
+ln -sf "$DOTFILES/spacevim/.SpaceVim.d" "$HOME/"
+
+#########
+# Fonts #
+#########
+
+mkdir -p "$XDG_DATA_HOME"
+cp -rf "$DOTFILES/fonts" "$XDG_DATA_HOME"
+
+#dunst notification system
+mkdir -p "$XDG_CONFIG_HOME/dunst"
+ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
 
