b1b73089726105d945dd362f4b1959ac8c36a626
Michael Weitner <michael@weitnerembeddedsystem.com>
2022-04-13 15:53:52 +0200

    added kill all workspace containers (reset) feature

---
+++ i3/scripts/kill-ws.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+i3-msg "workspace number 1 term; focus parent, focus parent, kill"
+i3-msg "workspace number 2 web; focus parent, focus parent, kill"
+i3-msg "workspace number 3 dev; focus parent, focus parent, kill"
+i3-msg "workspace number 4 vm; focus parent, focus parent, kill"
+
