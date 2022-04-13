c2b3a47946a6a9b0622ac58a24370cdbf9d1f460
Michael Weitner <michael@weitnerembeddedsystem.com>
2022-04-13 15:26:06 +0200

    fix workspace loading by layout i3 feature supporting default

--- i3/scripts/load-default-ws.sh
+++ i3/scripts/load-default-ws.sh
@@ -2,19 +2,24 @@
 
 set -e
 
-if [[ ! $(pgrep -u $UID -f "urxvt -name urxvt-terms") ]]; then
-  i3-msg "workspace number 1; append_layout $HOME/.config/i3/workspace1-terms.json"
-  # this did not work 
-  #i3-msg "exec --no-startup-id urxvt -name urxvt-terms"
-  . $HOME/.config/i3/scripts/start-urxvt.sh urxvt-terms
+if [[ ! $(pgrep -u $UID -f "urxvt -name urxvt-term") ]]; then
+  i3-msg "workspace number 1 term; append_layout $HOME/.config/i3/workspace1-term.json"
+  i3-msg "exec --no-startup-id $HOME/.config/i3/scripts/start-urxvt.sh urxvt-term"
+  # alternative is calling script directly but had strange behaviour
+  # where terminal was opened at dev workspace instead term workspace
+  # most likely a timing issue which is solved if i3 api is used!!!
+  #. $HOME/.config/i3/scripts/start-urxvt.sh urxvt-term
 fi
 if [[ ! $(pgrep -u $UID -x firefox) ]]; then
-  i3-msg "workspace number 2; append_layout $HOME/.config/i3/workspace2-web.json"
-  firefox &
-  ./$HOME/.config/i3/scripts/start-urxvt.sh urxvt-web
+  i3-msg "workspace number 2 web; append_layout $HOME/.config/i3/workspace2-web.json"
+  #  firefox &
+  i3-msg "exec --no-startup-id firefox"
+  i3-msg "exec --no-startup-id $HOME/.config/i3/scripts/start-urxvt.sh urxvt-web"
+  #. $HOME/.config/i3/scripts/start-urxvt.sh urxvt-web
 fi
 if [[ ! $(pgrep -u $UID -f "urxvt -name urxvt-dev") ]]; then
-  i3-msg "workspace number 3; append_layout $HOME/.config/i3/workspace3-dev.json"
-  . $HOME/.config/i3/scripts/start-urxvt.sh urxvt-dev
+  i3-msg "workspace number 3 dev; append_layout $HOME/.config/i3/workspace3-dev.json"
+  i3-msg "exec --no-startup-id $HOME/.config/i3/scripts/start-urxvt.sh urxvt-dev"
+  #. $HOME/.config/i3/scripts/start-urxvt.sh urxvt-dev
 fi
 
