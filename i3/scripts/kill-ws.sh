0aee86ea6d9fa9b758c8859321bdb173f54afe72
Michael Weitner <michael@weitnerembeddedsystem.com>
2022-04-15 20:42:50 +0200

    i3 improved workspace handling added kill all and zsh prompt changes

--- i3/scripts/kill-ws.sh
+++ i3/scripts/kill-ws.sh
@@ -2,6 +2,7 @@
 
 i3-msg "workspace number 1 term; focus parent, focus parent, kill"
 i3-msg "workspace number 2 web; focus parent, focus parent, kill"
-i3-msg "workspace number 3 dev; focus parent, focus parent, kill"
+#trick first kill workspace 4 than 3 so 3 is displayed last
 i3-msg "workspace number 4 vm; focus parent, focus parent, kill"
+i3-msg "workspace number 3 dev; focus parent, focus parent, kill"
 
